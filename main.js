41156272e1493882b97017bac6f90f82a7029fd7
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-18 17:16:20 +0100

    configure

--- main.js
+++ main.js
@@ -45,16 +45,38 @@ $(() => {
       toggleDivs(false);
     }
   });
-  $("#submit").click(() => {
+  $("form").submit((e) => {
+    e.preventDefault();
     if ($("#password").val().trim() == "") {
       $(".input-hold input").get(0).focus();
       $(".input-hold").addClass("input-hold-focus");
       $(".input-hold").addClass("input-required");
       $(".req").fadeIn(200);
       checkConditions();
-      e.preventDefault();
       return false;
     }
+    const form = e.target;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          window.location.href = "/welcome.html"; // Redirect to your custom thank-you page
+        } else {
+          alert("There was a problem with your submission. Please try again.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("There was a problem with your submission. Please try again.");
+      });
   });
+
   $("#back").click(() => toggleDivs(true));
 });
